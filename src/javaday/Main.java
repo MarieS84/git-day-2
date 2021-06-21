716460aa1b2b92970cace61f2d09551b8e1c7364
Marie Svensson <svenssonmc84@gmail.com>
2021-06-21 11:20:05 +0200

    testing

--- src/javaday/Main.java
+++ src/javaday/Main.java
@@ -1,5 +1,6 @@
 package javaday;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,27 +9,45 @@ public class Main {
     public static void main(String[] args) {
 
         ArrayList<Integer> numbers = new ArrayList<Integer>();
-            numbers.add(10);
-            numbers.add(30);
-            numbers.add(40);
-            numbers.add(50);
+        numbers.add(10);
+        numbers.add(30);
+        numbers.add(40);
+        numbers.add(50);
 
 
 
-        for (int i = 0; i < numbers.size(); i++ ){
             int lowest = 0;
-            if (i < lowest){
-                lowest = i;
+
+            for (int i = 1; i < numbers.size(); i++) {
+                lowest = numbers.get(0);
+
+                if (numbers.get(i) < lowest) {
+                    lowest = numbers.get(i);
+
+                }
 
             }
             System.out.println("Lowest number is " + lowest);
+
+        int highest = 0;
+
+        for (int i = 1; i < numbers.size(); i++) {
+            highest = numbers.get(0);
+
+            if (numbers.get(i) > highest) {
+                highest = numbers.get(i);
+
+            }
+
         }
-
+        System.out.println("Lowest number is " + highest);
 
         }
-
-
-
-
     }
+
+
+
+
+
+
 
